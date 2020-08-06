052538ea99137860760dc0f055843382d895b67d
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 14:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,4 @@ function subtract(){60 - 40}
 function multiply(){2 * 3.4}
 function divide(){5.0 / 2.5}
 var number = 10
-function add5(){number += 5}
-function divideBy3(){number /= 3}
-divideBy3()
-console.log(number)
-add5()
-console.log(number)
-number = 10
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
+function add(){number += 143}
