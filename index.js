110cd1e7a2d03de7d6aec11be8a87d0aa2693d86
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 15:06:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ function multiply(a,b){return a * b}
 function divide(a,b){return a / b}
 function increment(n){return n += 1}
 function decrement(n){return n-= 1}
+function makeInt(n){return parseInt(n, 10)}
