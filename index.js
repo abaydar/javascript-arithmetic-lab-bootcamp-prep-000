2c76723f2ce05c40c2ee33cca7791357b4ea1b1f
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 15:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function add(){1 + 80}
 function subtract(){60 - 40}
 function multiply(){2 * 3.4}
 function divide(){5.0 / 2.5}
-function add(a,b){return a + b}
+function addVar(a,b){return a + b}
+function subtractVar(a,b){return a - b}
+function multiplyVar(a,b){return a * b}
+function divideVar(a,b){return a / b}
+function increase(n){return increment(n)}
+function decrease(n){return decrement(n)}
